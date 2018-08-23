a5b78a79ec1b9d2e9c6b435da716485ae94a550e
mynameisarman <>
2018-08-23 01:08:05 -0400

    Using floats, using doubles

--- cpp-floats.cpp
+++ cpp-floats.cpp
@@ -5,10 +5,27 @@ using namespace std;
 
 int main(){
 
-  float fValue = 76.4; // Decimals must be declared with floats
+  // Using floats
+  float fValue = 123.456789; // Decimal values must be declared with floats or doubles
 
+  cout << "---- Floats ----" << endl;
   cout << "Float value: " << fValue << endl;
   cout << "Scientific float value: " << scientific << fValue << endl;
   cout << "Fixed float value: " << fixed << fValue << endl;
+  cout << "Size of float value: " << sizeof(float) << endl;
+
+
+  // Using doubles
+  double dValue = 123.456789;
+
+  cout << "---- Doubles ----" << endl;
+  cout << "Double value: " << dValue << endl;
+  cout << "Scientific double value: " << scientific << dValue << endl;
+  cout << "Fixed double value: " << fixed << dValue << endl;
+  cout << "Size of double value: " << sizeof(double) << endl;
+
+
+
+
   return 0;
 }
