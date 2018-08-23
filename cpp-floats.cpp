9bcb6f4aedb26a4887f4344188c9c6d88d7e9ef7
mynameisarman <>
2018-08-23 01:13:39 -0400

    Playing with long doubles

--- cpp-floats.cpp
+++ cpp-floats.cpp
@@ -11,7 +11,7 @@ int main(){
   cout << "---- Floats ----" << endl;
   cout << "Float value: " << fValue << endl;
   cout << "Scientific float value: " << scientific << fValue << endl;
-  cout << "Fixed float value: " << fixed << fValue << endl;
+  cout << "Fixed float value: " << setprecision(20) << fixed << fValue << endl;
   cout << "Size of float value: " << sizeof(float) << endl;
 
 
@@ -21,9 +21,18 @@ int main(){
   cout << "---- Doubles ----" << endl;
   cout << "Double value: " << dValue << endl;
   cout << "Scientific double value: " << scientific << dValue << endl;
-  cout << "Fixed double value: " << fixed << dValue << endl;
+  cout << "Fixed double value: " << setprecision(20) << fixed << dValue << endl;
   cout << "Size of double value: " << sizeof(double) << endl;
 
+
+  // Long double
+  long double lValue = 123.456789876543210;
+
+  cout << "- Long Doubles -" << endl;
+  cout << "Long double value: " << lValue << endl;
+  cout << "Scientific long double value: " << scientific << lValue << endl;
+  cout << "Fixed long double value: " << setprecision(20) << fixed << lValue << endl;
+  cout << "Size of long double value: " << sizeof(long double) << endl;
 
 
 
